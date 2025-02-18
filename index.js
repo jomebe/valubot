8ba0e31e3ec38dd07f5e50df1090e37d385d4268
jomebe <167706974+jomebe@users.noreply.github.com>
2025-02-18 21:57:45 +0900

    10->24시간

--- index.js
+++ index.js
@@ -994,7 +994,7 @@ client.on('messageCreate', async (message) => {
       return ['✅', '❌'].includes(reaction.emoji.name) && !user.bot;
     };
 
-    const collector = queueMessage.createReactionCollector({ filter, time: 36000000 }); // 1시간 동안 유지
+    const collector = queueMessage.createReactionCollector({ filter, time: 86400000 }); // 1시간 동안 유지
 
     collector.on('collect', async (reaction, user) => {
       const queue = getWaitingQueue(message.guild.id);
